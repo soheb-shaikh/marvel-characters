7207c463eb314bcf0ff15df3b3e751ae263b94d0
Soheb Shaikh <shaikhsoheb41@gmail.com>
2022-09-11 21:47:22 +0530

    feat: added trial code for swipe refresh. fix: kotlin version fix for compose compiler.

--- app/src/main/java/com/shaikhsoheb/marvelcharactersapp/view/MainActivity.kt
+++ app/src/main/java/com/shaikhsoheb/marvelcharactersapp/view/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.shaikhsoheb.marvelcharactersapp.ui.theme.MarvelCharactersAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    // Trial code for Swipe Refresh
+                    SwipeRefresh()
                 }
             }
         }
@@ -30,14 +34,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SwipeRefresh() {
+    val list = List(1000) { "$it"}
+    SwipeRefresh(
+        state = rememberSwipeRefreshState( true ),
+        onRefresh = {  },
+    ) {
+        LazyColumn {
+            items(list.size) { index ->
+                Text(text = index.toString())
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
     MarvelCharactersAppTheme {
-        Greeting("Android")
+        SwipeRefresh()
     }
 }
