a10cd1921f9ac59b71e653bf4bb823c3a9e4fb4d
Soheb Shaikh <shaikhsoheb41@gmail.com>
2022-09-11 19:14:42 +0530

    - Added API key, ts and hash for Marvel API.

---
+++ app/src/main/java/com/shaikhsoheb/marvelcharactersapp/data/remote/MarvelApiConstants.kt
@@ -0,0 +1,10 @@
+package com.shaikhsoheb.marvelcharactersapp.data.remote
+
+object MarvelApiConstants {
+
+    const val API_PUBLIC_KEY = "a6f186a3f9e791d99f7198f282941137"
+
+    const val API_TS = 1
+
+    const val API_HASH = "d29ba66d330b83fbeac15c2dab3bb899"
+}
